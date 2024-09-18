4fff0239c77743335a0becea37ee510b13324b6e
Darren Govoni <darren@visualagents.ai>
2024-09-18 12:24:20 -0400

    Updates

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 import sys
 
+import pymongo
 from fastapi import FastAPI
 
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
@@ -8,6 +9,6 @@ app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+async def hello():
     message = f"Hello world! From FastAPI running on Uvicorn with Gunicorn. Using Python {version}"
     return {"message": message}
